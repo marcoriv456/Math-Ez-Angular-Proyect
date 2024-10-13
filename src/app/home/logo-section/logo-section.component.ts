fe9a85146ed2eaa0fb2db3f7d19bd8a3c836fa88
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-13 02:42:00 -0600

    Logo section component created

---
+++ src/app/home/logo-section/logo-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-section',
+  templateUrl: './logo-section.component.html',
+  styleUrl: './logo-section.component.css'
+})
+export class LogoSectionComponent {
+
+}
