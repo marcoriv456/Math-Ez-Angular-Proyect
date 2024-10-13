65f4fb4e219a699ac24e20efc539b44e213eba60
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-13 02:58:36 -0600

    Introduction section component created

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from "./home.component";
 import {LogoSectionComponent} from "./logo-section/logo-section.component";
+import { IntroductionSectionComponent } from './introduction-section/introduction-section.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LogoSectionComponent
+    LogoSectionComponent,
+    IntroductionSectionComponent
   ],
   exports: [
     HomeComponent
---
+++ src/app/home/introduction-section/introduction-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introduction-section',
+  templateUrl: './introduction-section.component.html',
+  styleUrl: './introduction-section.component.css'
+})
+export class IntroductionSectionComponent {
+
+}
