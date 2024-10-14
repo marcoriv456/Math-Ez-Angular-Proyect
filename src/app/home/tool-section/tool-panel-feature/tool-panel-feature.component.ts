79eaa173d31d85e129ce65c0f82269b206a28fb2
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-13 19:52:01 -0600

    Tool panel feature icon linked with an input property
    The tool panel feature icon is now linked to an input property

--- src/app/home/tool-section/tool-panel-feature/tool-panel-feature.component.ts
+++ src/app/home/tool-section/tool-panel-feature/tool-panel-feature.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'home-tool-section-panel-feature',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './tool-panel-feature.component.css'
 })
 export class ToolPanelFeatureComponent {
-
+  @Input()
+  iconSrc!:string;
 }
