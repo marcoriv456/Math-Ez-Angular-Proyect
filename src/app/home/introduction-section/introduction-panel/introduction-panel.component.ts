9894d3289abd1871147caa5e49abe7ca2ceb307c
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-21 14:38:36 -0600

    Animations implemented in introduction section
    The scroll animations using intersection observer have been implemented in the introduction section panels

--- src/app/home/introduction-section/introduction-panel/introduction-panel.component.ts
+++ src/app/home/introduction-section/introduction-panel/introduction-panel.component.ts
@@ -1,13 +1,53 @@
-import {Component, Input} from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  Input,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
+import {SubjectIntersectionObserver} from "../../classes/subject-intersection-observer.class";
+import {IntersectionObserverSubject} from "../../models/IntersectionObserverSubject";
 
 @Component({
   selector: 'home-introduction-section-panel',
   templateUrl: './introduction-panel.component.html',
   styleUrl: './introduction-panel.component.css'
 })
-export class IntroductionPanelComponent {
+export class IntroductionPanelComponent implements AfterViewInit{
   @Input()
   sectionNumber!:number
   @Input()
   colorClass!:string
+
+  @ViewChild("panel__text")
+  panelText!:ElementRef;
+  @ViewChild("panel__illustration")
+  panelIllustration!:ElementRef
+
+  ref=inject(ElementRef)
+  renderer=inject(Renderer2)
+  intersectionObserver!:SubjectIntersectionObserver;
+  ngAfterViewInit() {
+    this.intersectionObserver=new SubjectIntersectionObserver(this.thisAsSubject,this.renderer)
+  }
+
+  get thisAsSubject():IntersectionObserverSubject{
+    return{
+      main:this.ref.nativeElement,
+      children:[
+        {
+          ref:this.panelText.nativeElement,
+          rootMargin:"-20% 0px -80% 0px"
+        },
+        {
+          ref:this.panelIllustration.nativeElement,
+          rootMargin:"0px 0px -100% 0px"
+        }
+      ]
+    }
+  }
+
 }
