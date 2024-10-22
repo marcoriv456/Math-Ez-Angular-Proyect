d2bd27f446175df649889f964210c1c6334da58f
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-22 15:19:08 -0600

    Tool section panel feature redesign

--- src/app/home/tool-section/tool-panel-feature/tool-panel-feature.component.ts
+++ src/app/home/tool-section/tool-panel-feature/tool-panel-feature.component.ts
@@ -8,4 +8,8 @@ import {Component, Input} from '@angular/core';
 export class ToolPanelFeatureComponent {
   @Input()
   iconSrc!:string;
+
+  get maskUrl(){
+    return `url("${this.iconSrc}")`;
+  }
 }
