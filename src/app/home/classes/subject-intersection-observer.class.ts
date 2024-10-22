c7d4518ff939367333686a0e1e3ebc3332622d10
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-21 19:16:29 -0600

    Page location service implementation in intersection observer
    The page location service has been implemented in the subject intersection observer class, so it can notify to the service if the page location has changed.

--- src/app/home/classes/subject-intersection-observer.class.ts
+++ src/app/home/classes/subject-intersection-observer.class.ts
@@ -1,8 +1,9 @@
 import {IntersectionObserverSubject} from "../models/IntersectionObserverSubject";
 import {Renderer2} from "@angular/core";
+import {PageLocationService} from "../services/page-location/page-location.service";
 
 export class SubjectIntersectionObserver {
-  constructor(subject:IntersectionObserverSubject, private renderer:Renderer2,) {
+  constructor(private subject:IntersectionObserverSubject, private renderer:Renderer2, private pageLocationServiceRef:PageLocationService) {
 
     this.observers.push(...subject.children.map(
       (child)=>{
@@ -13,7 +14,19 @@ export class SubjectIntersectionObserver {
         return observer
       }
     ))
+
+    this.observers.push(this.pagePositionObserver)
   }
+
+  private get pagePositionObserver(): IntersectionObserver{
+    let pagePosObserver= new IntersectionObserver((entries)=>{
+      if(this.getMainElementFrom(entries).isIntersecting)
+        this.pageLocationServiceRef.setPageLocation(this.subject.name)
+    })
+    pagePosObserver.observe(this.subject.main)
+    return pagePosObserver
+  }
+
   private observers:IntersectionObserver[]=[]
 
   private getMainElementFrom(entries:IntersectionObserverEntry[]){
