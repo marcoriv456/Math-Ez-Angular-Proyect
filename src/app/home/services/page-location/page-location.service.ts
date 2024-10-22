97430e817908d4bab451b0209f99b252d2a22e9d
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-21 19:06:46 -0600

    Page location service logic written

--- src/app/home/services/page-location/page-location.service.ts
+++ src/app/home/services/page-location/page-location.service.ts
@@ -4,4 +4,14 @@ import {EventEmitter, Injectable} from '@angular/core';
 export class PageLocationService {
 
   constructor() {}
+
+  private pageLocationEmitter=new EventEmitter<string>();
+  setPageLocation(pageLocation:string){
+    this.pageLocationEmitter.emit(pageLocation);
+  }
+
+  addListener(callback:(location:string)=>void){
+    this.pageLocationEmitter.subscribe(callback)
+  }
+
 }
