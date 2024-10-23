813fa027b41a44a0f523d7e97cea602bdd3be4ed
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-23 01:36:59 -0600

    Tab scrolling service implemented in tool panel feature

--- src/app/home/tool-section/tool-panel-feature/tool-panel-feature.component.ts
+++ src/app/home/tool-section/tool-panel-feature/tool-panel-feature.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, HostListener, inject, Input} from '@angular/core';
+import {SubjectIntersectionService} from "../../services/subject-intersection/subject-intersection.service";
+import {TabScrollingService} from "../../services/tab-scrolling/tab-scrolling.service";
 
 @Component({
   selector: 'home-tool-section-panel-feature',
@@ -8,8 +10,22 @@ import {Component, Input} from '@angular/core';
 export class ToolPanelFeatureComponent {
   @Input()
   iconSrc!:string;
+  private tabScrollingService=inject(TabScrollingService)
+  private ref=inject(ElementRef)
+
 
   get maskUrl(){
     return `url("${this.iconSrc}")`;
   }
+  @HostListener('focusin')
+  onFocus(){
+    let section=this.parentSection
+    if(!section)
+      return
+    this.tabScrollingService.moveScrollTo(section)
+  }
+  private get parentSection(){
+    return this.ref.nativeElement.closest("section")
+  }
+
 }
