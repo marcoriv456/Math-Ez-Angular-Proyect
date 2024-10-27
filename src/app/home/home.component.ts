82ae568882187c694ef66310edf9eb900513baf9
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-10-26 19:00:34 -0600

    Home service emitter logic created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, inject} from '@angular/core';
+import {HomeService} from "./services/home-service/home.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements AfterViewInit{
+  homeService=inject(HomeService)
+  ngAfterViewInit() {
+    this.homeService.homeInitialized.emit()
+  }
 }
--- src/app/home/services/home-service/home.service.ts
+++ src/app/home/services/home-service/home.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 
 @Injectable()
 export class HomeService {
 
   constructor() { }
-
+  homeInitialized=new EventEmitter()
 }
