092a3234c2864cb00f7cf56fdf0e23d3aca84afd
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-11-14 08:05:46 -0600

    Fraction deleted when user presses backspace and the denominator coponent is empty
    Now when the user presses backspace and the denominator component is empty, the fraction is deleted and replaced with the elements of the numerator component

--- src/app/input/fraction/denominator/denominator.component.ts
+++ src/app/input/fraction/denominator/denominator.component.ts
@@ -1,8 +1,19 @@
-import {Component, ElementRef, HostBinding, inject, Input, QueryList, ViewChildren} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  inject,
+  Input,
+  Output,
+  QueryList,
+  ViewChildren
+} from '@angular/core';
 import {InputEditableElement} from "../../classes/input-editable-element";
 import {InputTermDirective} from "../../directives/input-term.directive";
 import {Term} from "../../input.component";
 import {CaretPositioningService} from "../../services/caret-positioning/caret-positioning.service";
+import {InputCharData} from "../../char/char.component";
 
 @Component({
   selector: 'frac-denominator',
@@ -21,6 +32,8 @@ export class DenominatorComponent extends InputEditableElement{
   ref=inject(ElementRef).nativeElement as HTMLElement
   caretPositioningService=inject(CaretPositioningService)
 
+  @Output()
+  termDeleted=new EventEmitter<void>
 
   override get fontSize(): number {
     return super.fontSize/2;
@@ -30,5 +43,12 @@ export class DenominatorComponent extends InputEditableElement{
   @HostBinding('style.--font-size')
   override get fontSizeToBind(){
     return super.fontSizeToBind
+  }
+
+  override removeChar(from: number, deleteCount: number = 1): InputCharData|undefined {
+    if(this.terms.length)
+      return super.removeChar(from, deleteCount);
+    this.termDeleted.emit()
+    return;
   }
 }
