df8af9f8c0e0da7011b1cd2307840e647878fc43
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-11-24 22:20:12 -0600

    Fixed, strange behavior when adding a fraction
    The scroll was moving weird even if the caret was still visible

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -387,7 +387,7 @@ export class InputComponent extends InputEditableElement implements AfterViewIni
       denominatorChildren:nextChars,
       type:'fraction'
     })
-    this.moveCaretTo(this.getCharData(prevCharsFrom-1)||this.lastCharData)
+    this.moveCaretTo(this.currentElement.getCharData(prevCharsFrom-1)||this.currentElement.lastCharData)
     if(nextChars.length && prevChars.length)
       return;
     this.cdr.detectChanges()
