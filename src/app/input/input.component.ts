64a47f4f0b90186fabe21c637d18066222b49f28
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-11-29 21:44:28 -0600

    Insert something advice animation implemented

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -43,6 +43,15 @@ import {FunctionComponent} from "./components/function/function.component";
         animate('500ms cubic-bezier(0,0,0,1)',style({transform:'translateY(-100%)',opacity:0}))
       ]),
 
+    ]),
+    trigger('advice-fading',[
+      transition(':enter',[
+        style({transform:'translateY(-10%)',opacity:0}),
+        animate('500ms cubic-bezier(0,0,0,1)',style({transform:'translateY(0)',opacity:1}))
+      ]),
+      transition(':leave',[
+        animate('500ms cubic-bezier(0,0,0,1)',style({transform:'translateY(-10%)',opacity:0}))
+      ]),
     ])
   ]
 })
