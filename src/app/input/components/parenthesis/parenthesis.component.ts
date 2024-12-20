c9b03dce136d0d1a3a8ece897136da3319f82c86
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-12-19 19:20:49 -0600

    fix: Parenthesis children validation after being created
    The parenthesis children chars were skipping the validation after the component was added

--- src/app/input/components/parenthesis/parenthesis.component.ts
+++ src/app/input/components/parenthesis/parenthesis.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, inject, Input, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject, Input, ViewChild} from '@angular/core';
 import {InputEditableElement} from "../../models/input-editable-element.class";
 import {Term} from "../../models/terms/term.model";
 import {TermContainerComponent} from "../term-container/term-container.component";
@@ -10,7 +10,7 @@ import {InputCharData} from "../../models/input-char-data.model";
   templateUrl: './parenthesis.component.html',
   styleUrls: ['./parenthesis.component.css','../../assets/editable-elements-styles.css']
 })
-export class ParenthesisComponent extends InputEditableElement{
+export class ParenthesisComponent extends InputEditableElement implements AfterViewInit{
   @Input()
   terms!:Term[]
   @ViewChild(TermContainerComponent)
@@ -27,7 +27,11 @@ export class ParenthesisComponent extends InputEditableElement{
   protected override get positionX(): number {
     return super.positionX+this.leftParenthesis.nativeElement.offsetWidth;
   }
-  
+
+  ngAfterViewInit() {
+    this.updateTermsValidation()
+  }
+
   override removeChars(from: number, deleteCount: number = 1): InputCharData | undefined {
     if(this.inputUtilitiesService.caretIndex==-1)
       this.inputUtilitiesService.elementDeletedEmitter.emit({elementIndex:this.index, residualData:[...this.terms,{type:'char',char:')'}]})
