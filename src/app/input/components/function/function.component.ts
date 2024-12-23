4c6f1f23bc1d15d09b3f013d1b2a0aed0086af6c
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-12-22 20:49:06 -0600

    feat: Validation re-implemented in functions

--- src/app/input/components/function/function.component.ts
+++ src/app/input/components/function/function.component.ts
@@ -6,11 +6,12 @@ import {Term} from "../../models/terms/term.model";
 import {InputCharData} from "../../models/input-char-data.model";
 import {TermContainerComponent} from "../term-container/term-container.component";
 import {EditableTermContainerComponent} from "../editable-term-container/editable-term-container.component";
+import {WarningMessageData} from "../../models/char-validation/warning-message-data.model";
 
 @Component({
   selector: 'function',
   templateUrl: './function.component.html',
-  styleUrl: './function.component.css'
+  styleUrls: ['./function.component.css','../../assets/editable-elements-styles.css']
 })
 export class FunctionComponent extends InputEditableElement{
   @ViewChild(TermContainerComponent)
@@ -20,12 +21,9 @@ export class FunctionComponent extends InputEditableElement{
   functionName!:string
   @ViewChild('functionNameLabel')
   functionNameLabel!:ElementRef
-
   @Input()
   argumentTerms?:Term[]
-
   override editable=false
-
   @ViewChildren(InputTermDirective)
   _renderedChars!:QueryList<InputTermDirective>;
   override get renderedChars(): QueryList<InputTermDirective> {
@@ -35,5 +33,29 @@ export class FunctionComponent extends InputEditableElement{
   mainContainer!:EditableTermContainerComponent
   @ViewChild('functionArgument')
   argumentComponent!:EditableTermContainerComponent
+
+  protected override getValidationMessages(): WarningMessageData[] {
+    let validationMessages=super.getValidationMessages();
+    if(this.functionName=='log')
+      this.executeLogarithmValidations(validationMessages)
+    return validationMessages
+  }
+
+  private executeLogarithmValidations(messageList:WarningMessageData[]){
+    let argumentValue=this.argumentComponent.toString
+    this.validateTenAsLogarithmArgument(argumentValue,messageList)
+    this.validateLogarithmArgumentGreaterThanOne(argumentValue,messageList)
+  }
+
+  private validateTenAsLogarithmArgument(argumentValue:string,messageList:WarningMessageData[]){
+    if(argumentValue=='10')
+      messageList.push({type:'partially-invalid',message:'Aun si no se especifica, 10 es el valor por defecto de la base de un logaritmo.'})
+  }
+
+  private validateLogarithmArgumentGreaterThanOne(argumentValue:string,messageList:WarningMessageData[]){
+    let numberParentValue=Number(argumentValue)
+    if(!isNaN(numberParentValue) && !(numberParentValue>1))
+      messageList.push({type:'fully-invalid', message:'La base de un logaritmo debe de ser un numero positivo diferente de 1.'})
+  }
 }
 
