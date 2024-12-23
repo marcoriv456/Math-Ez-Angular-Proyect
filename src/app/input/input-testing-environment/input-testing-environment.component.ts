98f62eaf3a17ebc28db637d921cede6ee2831c62
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-12-23 15:40:37 -0600

    perf: Testing characters moved
    The testing character have been moved so they only appear if the input is showed in the testing environment

--- src/app/input/input-testing-environment/input-testing-environment.component.ts
+++ src/app/input/input-testing-environment/input-testing-environment.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {AfterViewInit, Component, inject, ViewChild, ViewChildren} from '@angular/core';
 import {ExperimentalVariableProviderService} from "./experimental-variable-provider.service";
 import {InputComponent} from "../input.component";
 
@@ -7,6 +7,65 @@ import {InputComponent} from "../input.component";
   templateUrl: './input-testing-environment.component.html',
   styleUrl: './input-testing-environment.component.css'
 })
-export class InputTestingEnvironmentComponent {
+export class InputTestingEnvironmentComponent implements AfterViewInit{
   expVariableProvider=inject(ExperimentalVariableProviderService)
+  @ViewChild(InputComponent)
+  input!:InputComponent
+
+  ngAfterViewInit() {
+    this.input.setTerms(
+      {char: '1', type: 'char'},
+      {char: '2', type: 'char'},
+      {char: '3', type: 'char'},
+      {char: '4', type: 'char'},
+      {char: '5', type: 'char'},
+      {char: '6', type: 'char'},
+      {char: '7', type: 'char'},
+      {char: '8', type: 'char'},
+      {char: '9', type: 'char'},
+      {char: ' ', type: 'char'},
+      {
+        numeratorChildren: [{char: '1', type: 'char'}, {char: '0', type: 'char'}],
+        denominatorChildren: [{char: '4', type: 'char'}],
+        type: 'fraction'
+      },
+      {char: ' ', type: 'char'},
+      {
+        functionName:'log',
+        functionChildren:[
+          {char: '8', type: 'char'},
+          {char: '1', type: 'char'},
+        ],
+        argumentTerms:[{char: '9', type: 'char'}],
+        type:'function'
+      },
+      {char: ' ', type: 'char'},
+      {
+        rootChildren:[
+          {char: '8', type: 'char'},
+          {char: '1', type: 'char'},
+        ],
+        radicalTerms:[{char:'9',type:'char'}],
+        type:'root'
+      },
+      {char: ' ', type: 'char'},
+      {
+        type:'fraction',
+        numeratorChildren:[
+          {char: '1', type: 'char'}
+        ],
+        denominatorChildren:[
+          {char: '8', type: 'char'},
+        ]
+      },
+      {char: ' ', type: 'char'},
+      {char: '8', type: 'char'},
+      {
+        exponentChildren:[
+          {char: '2', type: 'char'},
+        ],
+        type:'exponent'
+      }
+    )
+  }
 }
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -54,60 +54,7 @@ import {ParenthesisComponent} from "./components/parenthesis/parenthesis.compone
   ]
 })
 export class InputComponent implements AfterViewInit,OnInit {
-  protected terms: Term[] = [
-    {char: '1', type: 'char'},
-    {char: '2', type: 'char'},
-    {char: '3', type: 'char'},
-    {char: '4', type: 'char'},
-    {char: '5', type: 'char'},
-    {char: '6', type: 'char'},
-    {char: '7', type: 'char'},
-    {char: '8', type: 'char'},
-    {char: '9', type: 'char'},
-    {char: ' ', type: 'char'},
-    {
-      numeratorChildren: [{char: '1', type: 'char'}, {char: '0', type: 'char'}],
-      denominatorChildren: [{char: '4', type: 'char'}],
-      type: 'fraction'
-    },
-    {char: ' ', type: 'char'},
-    {
-      functionName:'log',
-      functionChildren:[
-        {char: '8', type: 'char'},
-        {char: '1', type: 'char'},
-      ],
-      argumentTerms:[{char: '9', type: 'char'}],
-      type:'function'
-    },
-    {char: ' ', type: 'char'},
-    {
-      rootChildren:[
-        {char: '8', type: 'char'},
-        {char: '1', type: 'char'},
-      ],
-      radicalTerms:[{char:'9',type:'char'}],
-      type:'root'
-    },
-    {char: ' ', type: 'char'},
-    {
-      type:'fraction',
-      numeratorChildren:[
-        {char: '1', type: 'char'}
-      ],
-      denominatorChildren:[
-        {char: '8', type: 'char'},
-      ]
-    },
-    {char: ' ', type: 'char'},
-    {char: '8', type: 'char'},
-    {
-      exponentChildren:[
-        {char: '2', type: 'char'},
-      ],
-      type:'exponent'
-    }
-  ];
+  protected terms: Term[] = [];
   private currentElement!: InputEditableElement;
   protected showingWarning=false
   protected warningMessages:TermWarningMessageData[]=[]
@@ -166,7 +113,7 @@ export class InputComponent implements AfterViewInit,OnInit {
     return [...this.terms]
   }
 
-  public setTerms(terms:Term[]){
+  public setTerms(...terms:Term[]){
     this.terms=terms
   }
   // ------------PUBLIC METHODS-------------
