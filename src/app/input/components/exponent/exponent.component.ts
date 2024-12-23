d4ae7efd15a2684906801eda0bfb6bc75f83e299
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2024-12-22 19:54:52 -0600

    feat: Validation re-implemented in exponents

--- src/app/input/components/exponent/exponent.component.ts
+++ src/app/input/components/exponent/exponent.component.ts
@@ -6,6 +6,7 @@ import {InputUtilitiesService} from "../../services/caret-positioning/input-util
 import {from} from "rxjs";
 import {InputCharData} from "../../models/input-char-data.model";
 import {TermContainerComponent} from "../term-container/term-container.component";
+import {WarningMessageData} from "../../models/char-validation/warning-message-data.model";
 
 @Component({
   selector: 'exp',
@@ -15,4 +16,24 @@ import {TermContainerComponent} from "../term-container/term-container.component
 export class ExponentComponent extends InputEditableElement{
   @ViewChild(TermContainerComponent)
   termContainer!: TermContainerComponent;
+
+  private zeroExponentRegexp=/^0+$/
+
+  protected override getValidationMessages(): WarningMessageData[] {
+    let validationMessages=super.getValidationMessages();
+    let actualValue=this.toString
+    this.validateOneExponent(actualValue,validationMessages)
+    this.validateZeroExponent(actualValue,validationMessages)
+    return validationMessages
+  }
+
+  private validateZeroExponent(actualValue:string, messageList:WarningMessageData[]){
+    if(this.zeroExponentRegexp.test(actualValue))
+      messageList.push({message:'Cualquier valor elevado a 0 es igual a 1.', type:'partially-invalid'})
+  }
+  
+  private validateOneExponent(actualValue:string, messageList:WarningMessageData[]){
+    if(actualValue=='1')
+      messageList.push({message:'Elevar a la potencia 1 es redundante.', type:'partially-invalid'})
+  }
 }
