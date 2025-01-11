8a71cf3b3bc99105de3c89ee8e607fbadaf2cb10
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-11 08:06:32 -0600

    refactor: Restructured root validator class created

---
+++ src/app/input/validation/validators/root.validator.ts
@@ -0,0 +1,58 @@
+import {RootComponent} from "../../components/root/root.component";
+import {TermValidator} from "../abstracts/validator.abstract";
+import {TermValidationMessage} from "../models/term-validation-message.model";
+
+export class RootValidator extends TermValidator{
+  private readonly radicandValue:number
+  private readonly indexValue:number
+  constructor(
+    rootComponent:RootComponent
+  ) {
+    super()
+    this.radicandValue = +rootComponent.mainComponent.toString
+    this.indexValue = +rootComponent.argumentComponent.toString
+  }
+
+  protected override getValidationMessages(): TermValidationMessage[] {
+    let messages:TermValidationMessage[]=[]
+
+    this.validateRadicandValue(messages)
+
+    if(this.indexValue)
+      this.validateIndexValue(messages)
+
+    return messages;
+  }
+
+  private validateRadicandValue(messages:TermValidationMessage[]) {
+    this.invalidateOneAsRadicandValue(messages)
+    this.invalidateNegativeRadicandAndOddIndex(messages)
+  }
+
+  private validateIndexValue(messages:TermValidationMessage[]) {
+    this.invalidateOneAsIndexValue(messages)
+    this.invalidateZeroAsIndexValue(messages)
+  }
+
+
+  private invalidateNegativeRadicandAndOddIndex(messages:TermValidationMessage[]){
+    if(this.radicandValue < 0 && (this.indexValue%2==0  || !this.indexValue))
+      messages.push({message:'La raiz de un numero negativo con indice par dara un numero complejo.'})
+  }
+
+  private invalidateOneAsRadicandValue(messages:TermValidationMessage[]) {
+    if(this.radicandValue==1)
+      messages.push({message:'La raiz de cualquier indice de 1 siempre sera 1.',type:'partially-invalid'})
+  }
+
+  private invalidateOneAsIndexValue(messages: TermValidationMessage[]) {
+    if(this.indexValue==1)
+      messages.push({message:'No se puede agregar 1 como indice de una raiz.',type:'fully-invalid'})
+  }
+
+  private invalidateZeroAsIndexValue(messages:TermValidationMessage[]) {
+    if(this.indexValue==0)
+      messages.push({message:'No se puede agregar 0 como indice de una raiz.',type:'fully-invalid'})
+  }
+  
+}
