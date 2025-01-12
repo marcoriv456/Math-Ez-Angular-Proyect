efbb32bd9ae93a827c76317ceebf22779c7a4f37
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-12 08:07:11 -0600

    fix: Root validator taking empty value as zero

--- src/app/input/validation/validators/root.validator.ts
+++ src/app/input/validation/validators/root.validator.ts
@@ -5,12 +5,14 @@ import {TermValidationMessage} from "../models/term-validation-message.model";
 export class RootValidator extends TermValidator{
   private readonly radicandValue:number
   private readonly indexValue:number
+  private readonly indexStringValue:string
   constructor(
     rootComponent:RootComponent
   ) {
     super()
+    this.indexStringValue = rootComponent.indexComponent.toString
     this.radicandValue = +rootComponent.radicandComponent.toString
-    this.indexValue = +rootComponent.indexComponent.toString
+    this.indexValue = +this.indexStringValue
   }
 
   protected override getValidationMessages(): TermValidationMessage[] {
@@ -36,7 +38,7 @@ export class RootValidator extends TermValidator{
 
 
   private invalidateNegativeRadicandAndOddIndex(messages:TermValidationMessage[]){
-    if(this.radicandValue < 0 && (this.indexValue%2==0  || !this.indexValue))
+    if(this.radicandValue < 0 && (this.indexValue%2==0  || this.indexStringValue==""))
       messages.push({message:'La raiz de un numero negativo con indice par dara un numero complejo.', type:'fully-invalid'})
   }
 
@@ -51,8 +53,8 @@ export class RootValidator extends TermValidator{
   }
 
   private invalidateZeroAsIndexValue(messages:TermValidationMessage[]) {
-    if(this.indexValue==0)
+    if(this.indexValue==0 && this.indexStringValue!=='')
       messages.push({message:'No se puede agregar 0 como indice de una raiz.',type:'fully-invalid'})
   }
-  
+
 }
