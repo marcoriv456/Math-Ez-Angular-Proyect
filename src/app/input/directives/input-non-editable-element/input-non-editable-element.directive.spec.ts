b065e36ef9b24fd18fb35147cd9dfc012d923bfb
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-12 10:16:47 -0600

    fix: Removed base classes test files
    These classes doesn't serve as a directive by itselfs, but rather as base classes.
    Because of this, these were causing problems with the other project's tests

--- src/app/input/directives/input-non-editable-element/input-non-editable-element.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputNonEditableElement } from './input-non-editable-element.directive';
-
-describe('InputNonEditableElementDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputNonEditableElement();
-    expect(directive).toBeTruthy();
-  });
-});
