b718b61d8cbda11a70f8711380ba44de1261e8c4
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-11 21:17:16 -0600

    refactor: Multiple refactors in inputEditableElements
    access modifiers explicitly declared
    reorganized functions and instance attributes
    formatting changes

--- src/app/input/directives/input-editable-element/input-editable-element.directive.ts
+++ src/app/input/directives/input-editable-element/input-editable-element.directive.ts
@@ -11,6 +11,7 @@ import {Subject} from "rxjs";
 
 @Directive()
 export abstract class InputEditableElement{
+
   protected abstract termContainer: TermContainerComponent
   @Input()
   public terms!:Term[]
@@ -18,8 +19,9 @@ export abstract class InputEditableElement{
   public parent!:InputEditableElement|undefined
   @Input()
   public index!:number
+
   public ref=inject(ElementRef).nativeElement as HTMLElement
-  public inputUtilitiesService=inject(CharClickedNotifierService)
+
   public editable=true
   public caretIndex=-1
 
@@ -29,91 +31,79 @@ export abstract class InputEditableElement{
   public validationRequester=new Subject<void>()
   protected validatorClass!:{ new (component: any): TermValidator }|undefined
 
-  get validator(){
+
+  public append(terms:Term[], index:number){
+    this.terms.splice(index,0,...terms)
+    this.updateValidation()
+  }
+
+  public replace(from:number, deleteCount=1,terms:Term[]){
+    this.terms.splice(from,deleteCount,...terms)
+    this.updateValidation()
+  }
+
+  public delete(from:number,deleteCount=1){
+    this.terms.splice(from,deleteCount)
+    this.updateValidation()
+  }
+
+  public get renderedChars(){
+    return this.termContainer.renderedChars
+  }
+
+  public get positionY(): number {
+    return this.ref.getBoundingClientRect().top;
+  }
+
+  public getRenderedChar(index:number){
+    return this.renderedChars.get(index)
+  }
+
+  public getCharData(index:number){
+    return this.getRenderedChar(index)?.data
+  }
+
+  public get nextCharData(){
+    return this.getCharData(this.caretIndex+1)||this.lastCharData
+  }
+
+  public get lastCharData(){
+    return this.renderedChars.get(this.renderedChars.length-1)?.data||this.noCharData
+  }
+
+  public get noCharData():InputCharData{
+    return {index:-1,positionX:this.positionX,positionY:this.positionY,size:this.size, parent:this}
+  }
+
+  public get nextIndex(){
+    return this.caretIndex+1
+  }
+
+  public get size():number{
+    return this.ref.offsetHeight
+  }
+
+  public get toString(){
+    return this.terms.map(child=> child.type=='char' ? child.char :'~').join('')
+  }
+
+  public get validator(){
     if(this.validatorClass)
       return new this.validatorClass(this)
     return;
   }
 
-  get renderedChars(){
-    return this.termContainer.renderedChars
+  public updateValidation(){
+    this.validationRequester.next()
   }
 
   protected get positionX(): number {
-    let parent:HTMLElement|null=this.ref.parentElement
+    let parent=this.ref.parentElement
     let leftPosition=this.ref.offsetLeft
     while(parent && !(parent.tagName=='APP-INPUT')){
-      leftPosition+=parent.offsetLeft||0
+      leftPosition+=parent.offsetLeft
       parent=parent.parentElement
     }
     return leftPosition
   }
-
-  get positionY(): number {
-    return this.ref.getBoundingClientRect().top;
-  }
-  getRenderedChar(index:number){
-    return this.renderedChars.get(index)
-  }
-
-  getCharData(index:number){
-    return this.getRenderedChar(index)?.data
-  }
-
-  get nextCharData(){
-    return this.getCharData(this.caretIndex+1)||this.lastCharData
-  }
-  get prevCharData(){
-    return this.getCharData(this.caretIndex-1)||this.noCharData
-  }
-
-  get lastCharData(){
-    return this.renderedChars.get(this.renderedChars.length-1)?.data||this.noCharData
-  }
-
-  get noCharData():InputCharData{
-    return {index:-1,positionX:this.positionX,positionY:this.positionY,size:this.size, parent:this}
-  }
-
-  get nextIndex(){
-    return this.caretIndex+1
-  }
-
-  get prevIndex(){
-    return this.caretIndex-1
-  }
-
-  get size():number{
-    return this.ref.offsetHeight
-  }
-
-  append(terms:Term[], index:number){
-    this.terms.splice(index,0,...terms)
-    this.updateValidation()
-  }
-
-  replace(from:number, deleteCount=1,terms:Term[]){
-    this.terms.splice(from,deleteCount,...terms)
-    this.updateValidation()
-  }
-
-  delete(from:number,deleteCount=1){
-    this.terms.splice(from,deleteCount)
-    this.updateValidation()
-  }
-
-  // ----------------------VALIDATION LOGIC----------------------
-  get toString(){
-    return this.terms.map(child=>{
-      if(child.type=='char')
-        return child.char
-      return '~'
-    }).join('')
-  }
-
-  updateValidation(){
-    this.validationRequester.next()
-  }
-
-  // ----------------------VALIDATION LOGIC----------------------
 }
