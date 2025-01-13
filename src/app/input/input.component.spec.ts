71f96c081783d835c19ff5b3c6e18d51017fec72
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-12 19:01:13 -0600

    test: Basic input component initialization tests created

--- src/app/input/input.component.spec.ts
+++ src/app/input/input.component.spec.ts
@@ -1,23 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputComponent } from './input.component';
+import {InputTestingEnvironmentComponent} from "./input-testing-environment/input-testing-environment.component";
+import {CharComponent} from "./components/char/char.component";
+import {FractionComponent} from "./components/fraction/fraction.component";
+import {InputTermDirective} from "./directives/input-term/input-term.directive";
+import {FractionChildComponent} from "./components/fraction/fraction-child/fraction-child.component";
+import {ExponentComponent} from "./components/exponent/exponent.component";
+import {RootComponent} from "./components/root/root.component";
+import {FunctionComponent} from "./components/function/function.component";
+import {TermContainerComponent} from "./components/term-container/term-container.component";
+import {TermArgumentComponent} from "./components/term-argument/term-argument.component";
+import {EditableTermContainerComponent} from "./components/editable-term-container/editable-term-container.component";
+import {ParenthesisComponent} from "./components/parenthesis/parenthesis.component";
+import {TermValidationWarningComponent} from "./components/term-validation-warning/term-validation-warning.component";
+import {CommonModule} from "@angular/common";
+import {CharClickedNotifierService} from "./services/char-clicked-notifier/char-clicked-notifier.service";
+import {WarningsService} from "./services/warnings/warnings.service";
+import {VariableProviderService} from "./services/variable-provider/variable-provider.service";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Term} from "./models/terms/term.model";
+import {VariableProvider} from "./models/variable-provider.model";
+import {ExperimentalVariableProviderService} from "./input-testing-environment/experimental-variable-provider.service";
+import {inject} from "@angular/core";
 
-describe('InputComponent', () => {
+fdescribe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
 
+  let variableProvider = new ExperimentalVariableProviderService()
+  let variableProviderContainer: VariableProviderService;
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [InputComponent]
+      declarations: [
+        InputComponent,
+        InputTestingEnvironmentComponent,
+        CharComponent,
+        FractionComponent,
+        InputTermDirective,
+        FractionChildComponent,
+        ExponentComponent,
+        RootComponent,
+        FunctionComponent,
+        TermContainerComponent,
+        TermArgumentComponent,
+        EditableTermContainerComponent,
+        ParenthesisComponent,
+        TermValidationWarningComponent
+      ],
+      imports: [
+        CommonModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        CharClickedNotifierService,
+        WarningsService,
+        VariableProviderService,
+      ],
+
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(InputComponent);
+    fixture.componentRef.setInput('variableProvider', variableProvider)
+
+    variableProviderContainer = TestBed.inject(VariableProviderService)
+    spyOn(variableProviderContainer, 'setVariableProvider').and.callThrough()
+
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  //--------------------- INITIALIZATION TESTS---------------------
+  describe('Component initialization', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should init variable provider object', () => {
+      expect(component.variableProvider).toBeTruthy()
+    })
+
+    it('should set the variable provider in the container service', () => {
+      expect(variableProviderContainer.setVariableProvider).toHaveBeenCalled()
+    });
+  })
+  //--------------------- INITIALIZATION TESTS---------------------
+})
