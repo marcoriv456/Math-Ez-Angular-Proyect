18d36950a48aed3122a20b56604a9230c47aa3b1
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-14 21:36:09 -0600

    test: Char component test fixed

--- src/app/input/components/char/char.component.spec.ts
+++ src/app/input/components/char/char.component.spec.ts
@@ -1,17 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharComponent } from './char.component';
+import {VariableProvider} from "../../models/variable-provider.model";
+import {VariableProviderService} from "../../services/variable-provider/variable-provider.service";
+import {
+  ExperimentalVariableProviderService
+} from "../../input-testing-environment/experimental-variable-provider.service";
+import {By} from "@angular/platform-browser";
 
 describe('CharComponent', () => {
   let component: CharComponent;
   let fixture: ComponentFixture<CharComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CharComponent]
+      declarations: [CharComponent],
+      providers:[VariableProviderService]
     })
     .compileComponents();
-
     fixture = TestBed.createComponent(CharComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +25,5 @@ describe('CharComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
