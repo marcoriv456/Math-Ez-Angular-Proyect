00842d3fa60b2b441aef5b0164da91f847631213
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-18 18:54:16 -0600

    test: Configure TestBed helper created
    This function should serve as an interface to configure the TestBed start the input's elements tests faster

---
+++ src/app/input/tests/config/configure-testbed.helper.ts
@@ -0,0 +1,18 @@
+import {TestBed} from "@angular/core/testing";
+import {testingModuleConfig} from "./testing-module.config";
+import {VariableProviderService} from "../../services/variable-provider/variable-provider.service";
+import {
+  ExperimentalVariableProviderService
+} from "../../input-testing-environment/experimental-variable-provider.service";
+
+export const configureTestbed= async (type:{new (...args:any[]):any}) => {
+  await TestBed.configureTestingModule(testingModuleConfig)
+    .compileComponents();
+
+  let fixture = TestBed.createComponent(type);
+  let component = fixture.componentInstance;
+
+  fixture.detectChanges();
+  TestBed.inject(VariableProviderService).setVariableProvider(new ExperimentalVariableProviderService())
+  return {testBedFixture:fixture,fixtureComponent:component}
+}
