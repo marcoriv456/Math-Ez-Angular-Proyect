a3aa13009bfae4fa0b9884aff5d84f1ec4da3106
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-18 18:44:18 -0600

    test: Editable term container tests implemented

--- src/app/input/components/editable-term-container/editable-term-container.component.spec.ts
+++ src/app/input/components/editable-term-container/editable-term-container.component.spec.ts
@@ -1,23 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { EditableTermContainerComponent } from './editable-term-container.component';
+import {EditableTermContainerComponent} from './editable-term-container.component';
+import {runEditableElementTests} from "../../tests/editable-elements-shared-tests.spec..test";
+import {testingModuleConfig} from "../../tests/config/testing-module.config";
+import {VariableProviderService} from "../../services/variable-provider/variable-provider.service";
+import {
+  ExperimentalVariableProviderService
+} from "../../input-testing-environment/experimental-variable-provider.service";
 
 describe('EditableTermContainerComponent', () => {
   let component: EditableTermContainerComponent;
   let fixture: ComponentFixture<EditableTermContainerComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [EditableTermContainerComponent]
-    })
+    await TestBed.configureTestingModule(testingModuleConfig)
     .compileComponents();
 
     fixture = TestBed.createComponent(EditableTermContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    TestBed.inject(VariableProviderService).setVariableProvider(new ExperimentalVariableProviderService())
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  runEditableElementTests<EditableTermContainerComponent>('EditableTermContainerComponent')
+
 });
