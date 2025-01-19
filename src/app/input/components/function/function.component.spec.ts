fd411fb2fbdca71543ba18b4693738b7eb8263d2
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-18 20:37:14 -0600

    test: Function component tests

--- src/app/input/components/function/function.component.spec.ts
+++ src/app/input/components/function/function.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FunctionComponent } from './function.component';
+import {configureTestbed} from "../../tests/config/configure-testbed.helper";
+import {runEditableElementTests} from "../../tests/editable-elements-shared-tests.spec..test";
+import {EditableTermContainerComponent} from "../editable-term-container/editable-term-container.component";
 
 describe('FunctionComponent', () => {
   let component: FunctionComponent;
   let fixture: ComponentFixture<FunctionComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [FunctionComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FunctionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    let {_fixture,_component}=await configureTestbed(FunctionComponent)
+    component=_component
+    fixture=_fixture
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the argument container', () => {
+    expect(component.argumentContainer).toBeTruthy()
+  });
+
+  it('should have just one element', () => {
+    expect(component.renderedChars.length).toBe(1);
+  });
+
+  describe('When the function has base terms: ', () => {
+    beforeEach(()=>{
+      fixture.componentRef.setInput('baseTerms', [])
+      fixture.detectChanges()
+    })
+
+    it('should initialize the base container', () => {
+      expect(component.baseComponent).toBeTruthy()
+    })
+  })
+
 });
