baa00c69b3991d6800505e2ebff960669172355b
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-18 20:51:37 -0600

    test: Root component tests created

--- src/app/input/components/root/root.component.spec.ts
+++ src/app/input/components/root/root.component.spec.ts
@@ -1,16 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RootComponent } from './root.component';
+import {configureTestbed} from "../../tests/config/configure-testbed.helper";
 
 describe('RootComponent', () => {
   let component: RootComponent;
   let fixture: ComponentFixture<RootComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [RootComponent]
-    })
-    .compileComponents();
+    await configureTestbed(RootComponent)
 
     fixture = TestBed.createComponent(RootComponent);
     component = fixture.componentInstance;
@@ -20,4 +18,24 @@ describe('RootComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should only have one component', () => {
+    expect(component.renderedChars.length).toBe(1);
+  });
+
+  describe('when theres radical terms defined ', () => {
+    beforeEach(()=>{
+      fixture.componentRef.setInput('radicalTerms',[])
+      fixture.detectChanges();
+    })
+
+    it('should only have two components', () => {
+      expect(component.renderedChars.length).toBe(2)
+    });
+
+    it('should initialize index component', () => {
+      expect(component.indexComponent).toBeTruthy()
+    });
+  })
+
 });
