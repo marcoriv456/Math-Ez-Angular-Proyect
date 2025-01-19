0aa8ac971d5f925d163f6b1cf24e8440fc7ef676
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-18 20:53:38 -0600

    test: Term argument component tests

--- src/app/input/components/term-argument/term-argument.component.spec.ts
+++ src/app/input/components/term-argument/term-argument.component.spec.ts
@@ -1,16 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TermArgumentComponent } from './term-argument.component';
+import {configureTestbed} from "../../tests/config/configure-testbed.helper";
+import {runEditableElementTests} from "../../tests/editable-elements-shared-tests.spec..test";
 
 describe('TermArgumentComponent', () => {
   let component: TermArgumentComponent;
   let fixture: ComponentFixture<TermArgumentComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TermArgumentComponent]
-    })
-    .compileComponents();
+    await configureTestbed(TermArgumentComponent)
 
     fixture = TestBed.createComponent(TermArgumentComponent);
     component = fixture.componentInstance;
@@ -20,4 +19,6 @@ describe('TermArgumentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  runEditableElementTests('TermArgument', TermArgumentComponent)
 });
