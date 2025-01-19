e613179fc4c148a8a1c0e21c251550064dac1b4e
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-18 20:15:42 -0600

    test: Fraction component tests

--- src/app/input/components/fraction/fraction.component.spec.ts
+++ src/app/input/components/fraction/fraction.component.spec.ts
@@ -1,23 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FractionComponent } from './fraction.component';
+import {configureTestbed} from "../../tests/config/configure-testbed.helper";
 
 describe('FractionComponent', () => {
   let component: FractionComponent;
   let fixture: ComponentFixture<FractionComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [FractionComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FractionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    let {_fixture,_component}=await configureTestbed(FractionComponent)
+    component=_component
+    fixture=_fixture
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the numerator initialized', () => {
+    expect(component.numeratorComponent).toBeTruthy()
+  })
+  it('should have the denominator initialized', () => {
+    expect(component.denominatorComponent).toBeTruthy()
+  })
+
+  it('should only have 2 children', () => {
+    expect(component.renderedChars.length).toBe(2);
+  });
+
 });
