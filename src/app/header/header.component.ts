0f7b49eaae6f394c1418ccaae291a7acd2642aa7
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-21 20:15:53 -0600

    feat: Header morphing animations fully implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject, OnDestroy} from '@angular/core';
+import {interpolate} from "flubber";
+import {FlubberAnimation} from "./helpers/flubber-animation.helper";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css','../../assets/styles/section-color-themes.css']
 })
-export class HeaderComponent { }
+export class HeaderComponent implements AfterViewInit,OnDestroy{
+  private flubberAnimations:FlubberAnimation[]=[]
+  private flubberAnimationDuration=2000
+
+  private addFlubberAnimation(layer:number){
+    let animation=new FlubberAnimation(`.step-1 .layer-${layer}`, `.step-2 .layer-${layer}`, `.step-3 .layer-${layer}`)
+    this.flubberAnimations.push(animation)
+    animation.start(this.flubberAnimationDuration)
+  }
+
+  ngAfterViewInit() {
+    for (let i=1; i<=4; i++)
+      this.addFlubberAnimation(i)
+  }
+  ngOnDestroy() {
+    this.flubberAnimations.forEach(a=>a.stop())
+  }
+}
