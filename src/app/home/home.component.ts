16c4cbf302027ea5951afac5e0040e82c4666de0
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-23 09:54:50 -0600

    feat: Topics section with blobs animations implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+  @ViewChild('topicsSection') topicsSection!:ElementRef;
+
+  private renderer=inject(Renderer2)
+  private observerCallback= (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting)
+        this.renderer.addClass(entry.target, 'intersecting')
+      else
+        this.renderer.removeClass(entry.target, 'intersecting');
+    })
+  }
+  private intersectionObserver=new IntersectionObserver(this.observerCallback,{threshold:0.7})
+
+  ngAfterViewInit() {
+    this.intersectionObserver.observe(this.topicsSection.nativeElement)
+  }
+
 
 }
