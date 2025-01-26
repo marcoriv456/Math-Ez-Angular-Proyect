5762b09b0bbc9e765b23c51997d65f0cde935529
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-26 08:40:31 -0600

    feat: Old footer structure and files removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { InputComponent } from './input/input.component';
 import { InputTestingEnvironmentComponent } from './input/input-testing-environment/input-testing-environment.component';
 import {InputModule} from "./input/input.module";
@@ -16,7 +15,6 @@ import {NgxParticlesModule} from "@tsparticles/angular";
     declarations: [
         AppComponent,
         HeaderComponent,
-        FooterComponent,
         HomeComponent
     ],
     imports: [
