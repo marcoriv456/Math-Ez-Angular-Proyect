f0d5b0f80353838e137537134882aa91a34df293
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-26 19:57:15 -0600

    test: Experimental variable provider tests created

--- src/app/input/input-testing-environment/experimental-variable-provider.service.spec.ts
+++ src/app/input/input-testing-environment/experimental-variable-provider.service.spec.ts
@@ -13,4 +13,17 @@ describe('ExperimentalVariableProviderService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have some defaul variable initialized', () => {
+    expect(service.variables).toBeTruthy()
+  });
+
+  it('should return its variable names', () => {
+    expect(service.variableNames).toEqual(['A','B','C'])
+  });
+  it('should return its variable values', () => {
+    expect(service.getVariable('A')).toEqual(service.variables.get('A'))
+    expect(service.getVariable('B')).toEqual(service.variables.get('B'))
+    expect(service.getVariable('C')).toEqual(service.variables.get('C'))
+  });
 });
