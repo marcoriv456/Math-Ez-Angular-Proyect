79f99e8942a8d037e30365254328d1ee2c36a29c
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-26 20:17:48 -0600

    test: WarningsService tests created

--- src/app/input/services/warnings/warnings.service.spec.ts
+++ src/app/input/services/warnings/warnings.service.spec.ts
@@ -6,11 +6,19 @@ describe('WarningsService', () => {
   let service: WarningsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({providers:[WarningsService]});
     service = TestBed.inject(WarningsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have initialized the showWarning emitter', () => {
+    expect(service.showWarning).toBeTruthy()
+  });
+
+  it('should have initialized the hideWarning emitter', () => {
+    expect(service.hideWarning).toBeTruthy()
   });
 });
