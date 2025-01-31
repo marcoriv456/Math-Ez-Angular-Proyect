f4775d48fcf0fe68344d35af1718118e66bf8fa4
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-30 21:16:21 -0600

    test: Exponent validator restructuring
    The test suite was poor, presenting the same problems as the previous restructured test had

--- src/app/input/validation/tests/exponent.validator.spec.ts
+++ src/app/input/validation/tests/exponent.validator.spec.ts
@@ -1,120 +1,51 @@
 import {ExponentValidator} from "../validators/exponent.validator";
 import {ExponentComponent} from "../../components/exponent/exponent.component";
 import {TermValidationData} from "../models/term-validation-data.model";
+import {validTermValidation} from "../default-values/valid-term-validation";
 
 describe("Exponent validator: ", () => {
-  const getExponentComponent=(expValue:string)=>{
-    return {
+  const getExponentComponent=(expValue:string)=>( {
       get toString(){
         return expValue
       }
-    } as unknown as ExponentComponent
-  }
-  let validator:ExponentValidator
-  describe('Validator initializing: ',()=>{
-    it('should initialize the string value', () => {
-      validator=new ExponentValidator(getExponentComponent('12'))
+    } as unknown as ExponentComponent)
+  const getExponentValidatorOf = (expValue:string) => new ExponentValidator(getExponentComponent(expValue))
+  const validData=validTermValidation
+
+  it('Empty value is valid', () => {
+    const validator=new ExponentValidator(getExponentComponent(''))
+
+    const validationData = validator.validate()
+
+    expect(validationData).toEqual(validData)
+  });
 
 
-      expect(validator['stringValue']).toBe('12')
-    });
-    it('should initialize the value', () => {
-      validator=new ExponentValidator(getExponentComponent('12'))
+  it('A not numerical value is valid', () => {
+    const validator=getExponentValidatorOf('A+B')
 
-      expect(validator['value']).toBe(12)
-    });
+    const validationData = validator.validate()
 
+    expect(validationData).toEqual(validData)
+  });
 
-    it('should not initialize the number value', () => {
-      validator=new ExponentValidator(getExponentComponent('A+B'))
+  it('One as value is partially invalid', () => {
+    const validator=getExponentValidatorOf('1')
 
-      expect(validator['value']).toBeNaN()
-    });
+    const validationData = validator.validate()
 
-  })
+    expect(validationData.isValid).toBeFalse()
+    expect(validationData.type).toBe('partially-invalid')
+    expect(validationData.messages.length).toBe(1)
+  });
 
-  describe('Validation on invalid value: ',()=>{
-    it('should return an empty array', () => {
-      validator=new ExponentValidator(getExponentComponent('A+B'))
+  it('Zero as value is partially invalid', () => {
+    const validator=getExponentValidatorOf('0')
 
-      expect(validator['getValidationMessages']()).toEqual([])
-    });
-    it('should not execute the validation', () => {
-      validator=new ExponentValidator(getExponentComponent('A+B'))
-      spyOn(validator as any, 'validateOneExponent')
-      spyOn(validator as any, 'validateZeroExponent')
+    const validationData= validator.validate()
 
-      expect(validator['validateOneExponent']).not.toHaveBeenCalled()
-      expect(validator['validateZeroExponent']).not.toHaveBeenCalled()
-    });
-  })
-
-  describe('Validation process: ', () => {
-    let validationData:TermValidationData
-    describe('Given 1 as value: ',()=>{
-      beforeEach(()=>{
-        validator=new ExponentValidator(getExponentComponent('1'))
-        validationData=validator.validate()
-      })
-
-      it('should return "isValid" as false', () => {
-        expect(validationData.isValid).toBeFalse()
-      });
-
-      it('should return "type" as "partially-invalid"', () => {
-        expect(validationData.type).toBe('partially-invalid')
-      });
-
-      it('should return an array of one message ', () => {
-        expect(validationData.messages.length).toBe(1)
-      });
-
-      it('should return the following message', () => {
-        expect(validationData.messages[0]).toEqual({message:'Elevar a la potencia 1 es redundante.',type:'partially-invalid'})
-      });
-    })
-
-    describe('Given 0 as value:', () => {
-      beforeEach(()=>{
-        validator=new ExponentValidator(getExponentComponent('0'))
-        validationData=validator.validate()
-      })
-
-      it('should return "isValid" as false', () => {
-        expect(validationData.isValid).toBeFalse()
-      });
-
-      it('should return "type" as "partially-invalid"', () => {
-        expect(validationData.type).toBe('partially-invalid')
-      });
-
-      it('should return an array of one message ', () => {
-        expect(validationData.messages.length).toBe(1)
-      });
-
-      it('should return the following message', () => {
-        expect(validationData.messages[0]).toEqual({message:'Cualquier valor elevado a 0 es igual a 1.',type:'partially-invalid'})
-      });
-
-    });
-
-    describe('Given an empty string as value', () => {
-      beforeEach(()=>{
-        validator=new ExponentValidator(getExponentComponent(''))
-        validationData=validator.validate()
-      })
-
-      it('should return "isValid" as true', () => {
-        expect(validationData.isValid).toBeTrue()
-      });
-
-      it('should not return nothing as "type"', () => {
-        expect(validationData.type).not.toBeDefined()
-      });
-
-      it('should return an empty messages array ', () => {
-        expect(validationData.messages.length).toBe(0)
-      });
-    })
+    expect(validationData.isValid).toBeFalse()
+    expect(validationData.type).toBe('partially-invalid')
+    expect(validationData.messages.length).toBe(1)
   });
 })
