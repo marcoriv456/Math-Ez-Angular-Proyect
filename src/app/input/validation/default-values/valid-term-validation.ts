57e835ebab2e84a9e0fa3514274a460b45991569
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-01-30 21:15:34 -0600

    test: ValidTermValidation undefined type explicitly declared
    The change was to enable this constant to be useful not only in production code but also in tests

--- src/app/input/validation/default-values/valid-term-validation.ts
+++ src/app/input/validation/default-values/valid-term-validation.ts
@@ -2,5 +2,6 @@ import {TermValidationData} from "../models/term-validation-data.model";
 
 export const validTermValidation:TermValidationData={
   isValid:true,
-  messages:[]
+  messages:[],
+  type:undefined
 }
