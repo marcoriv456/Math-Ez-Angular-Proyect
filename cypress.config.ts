aaefa824429e574e1f29b4b91f87d60dd4dd5965
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-01 17:52:36 -0600

    test: Cypress implemented and configurated
    Now the installation was made using the angular CLI so it can configure cypress to specially run with the framework configurations and components, this allows me to do angular component GUI testing on important components

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+
+  e2e: {
+    'baseUrl': 'http://localhost:4200',
+    viewportWidth:1920,
+    viewportHeight:1080
+  },
+
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts'
+  },
+  viewportWidth:1920,
+  viewportHeight:1080
+})
