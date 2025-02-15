c2b7dc8290117e357ea5aff346ad9676e167465e
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-15 12:10:36 -0600

    test: Exponent component gui tests

---
+++ src/app/input/components/exponent/exponent.component.cy.ts
@@ -0,0 +1,5 @@
+import {runEditableElementSuite} from "../../tests/run-editable-element-tests-suite.helper";
+
+describe('Exponent component: ', () => {
+  runEditableElementSuite("Exponent component:","{ctrl}e","exp")
+});
