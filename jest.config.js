0cb61a0079927ebbef1c1d2b1a23d8da98012a60
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-16 10:41:56 -0600

    test: Chai implemented as assertion library for jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '^.+\\.(ts|mjs|html)$': 'ts-jest',
   },
   testMatch: ['**/__tests__/**/*.spec.ts', '**/*.spec.ts'],
-  transformIgnorePatterns: ['node_modules/(?!@angular|rxjs)'],
+  transformIgnorePatterns: ['node_modules/(?!(@angular|rxjs|chai)/)'],
   moduleNameMapper: {
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
