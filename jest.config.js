a19930d27f7ac157da3955cf876602ae22ed1df7
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-16 09:57:52 -0600

    test: Jest deprecated configuration removed

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   preset: 'jest-preset-angular',
+  transform: {
+    '^.+\\.(ts|mjs|html)$': 'ts-jest',
+  },
   testMatch: ['**/__tests__/**/*.spec.ts', '**/*.spec.ts'],
   transformIgnorePatterns: ['node_modules/(?!@angular|rxjs)'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-    },
-  },
   moduleNameMapper: {
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
