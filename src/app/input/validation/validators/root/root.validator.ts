2dde9aa35d3db9ae2c0cb2df1e37d4e5dc4598e0
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-16 20:02:56 -0600

    refactor: Root validator refactoring

--- src/app/input/validation/validators/root/root.validator.ts
+++ src/app/input/validation/validators/root/root.validator.ts
@@ -1,17 +1,23 @@
 import {RootComponent} from "../../../components/root/root.component";
 import {TermValidator} from "../../abstracts/validator.abstract";
 import {TermValidationMessage} from "../../models/term-validation-message.model";
+import {Term} from "../../../models/terms/term.model";
+import {UnexpectedTermTypeError} from "../../errors/unexpected-term-type.error";
+import {TermUtils} from "../../../classes/term-utils";
 
 export class RootValidator extends TermValidator{
   private readonly radicandValue:number
   private readonly indexValue:number|undefined
   private readonly indexStringValue:string|undefined
   constructor(
-    rootComponent:RootComponent
+    term:Term
   ) {
     super()
-    this.indexStringValue = rootComponent.indexComponent?.toString
-    this.radicandValue = +rootComponent.radicandComponent.toString
+    if(term.type!=='root')
+      throw new UnexpectedTermTypeError("root",term.type)
+
+    this.indexStringValue = TermUtils.toString(term.radicalTerms||[])
+    this.radicandValue = +TermUtils.toString(term.rootChildren)
     this.indexValue = +this.indexStringValue
   }
 
