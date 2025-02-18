b3a06e32ffb867a42d7579cf22f5ecb8145efdd5
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-18 14:37:47 -0600

    refactor: Exponent component extends from InputMathElement
    The component's view works fine when implementing the new architecture

--- src/app/input/components/exponent/exponent.component.ts
+++ src/app/input/components/exponent/exponent.component.ts
@@ -2,13 +2,12 @@ import {Component, ViewChild} from '@angular/core';
 import {TermContainerComponent} from "../term-container/term-container.component";
 import {InputEditableElement} from "../../directives/input-editable-element/input-editable-element.directive";
 import {ExponentValidator} from "../../validation/validators/exponent/exponent.validator";
+import {InputMathElement} from "../../directives/input-math-element/input-math-element.abstract";
+import {ExponentTerm} from "../../models/terms/exponent-term";
 
 @Component({
   selector: 'exp',
   templateUrl: './exponent.component.html',
   styleUrls: ['./exponent.component.css','../../assets/editable-elements-styles.css']
 })
-export class ExponentComponent extends InputEditableElement{
-  @ViewChild(TermContainerComponent)
-  termContainer!: TermContainerComponent;
-}
+export class ExponentComponent extends InputMathElement<ExponentTerm>{ }
