ffbcaf1a1ca915f350a73c81a206c19d831b73e6
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-18 13:39:28 -0600

    refactor: Function component extends from inputMathElement
    Its view works correctly when implementing the new architecture

--- src/app/input/components/function/function.component.ts
+++ src/app/input/components/function/function.component.ts
@@ -1,33 +1,11 @@
-import {Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {InputTermDirective} from "../../directives/input-term/input-term.directive";
-import {Term} from "../../models/terms/term.model";
-import {TermContainerComponent} from "../term-container/term-container.component";
-import {EditableTermContainerComponent} from "../editable-term-container/editable-term-container.component";
-import {InputEditableElement} from "../../directives/input-editable-element/input-editable-element.directive";
-import {LogarithmValidator} from "../../validation/validators/functions/logarithm/logarithm.validator";
-import {
-  InputNonEditableElement
-} from "../../directives/input-non-editable-element/input-non-editable-element.directive";
+import {Component} from '@angular/core';
+import {InputMathElement} from "../../directives/input-math-element/input-math-element.abstract";
+import {FunctionTerm} from "../../models/terms/function-term.model";
 
 @Component({
   selector: 'function',
   templateUrl: './function.component.html',
   styleUrls: ['./function.component.css','../../assets/editable-elements-styles.css']
 })
-export class FunctionComponent extends InputNonEditableElement{
-  @Input()
-  functionName!:string
-  @ViewChild('functionNameLabel')
-  functionNameLabel!:ElementRef
-  @Input()
-  baseTerms?:Term[]
-  override get renderedChars(): QueryList<InputTermDirective> {
-    return this._renderedChars
-  }
-  @ViewChild('functionArgumentContainer')
-  argumentContainer!:EditableTermContainerComponent
-  @ViewChild('functionBaseComponent')
-  baseComponent!:EditableTermContainerComponent
-
-}
+export class FunctionComponent extends InputMathElement<FunctionTerm> {}
 
