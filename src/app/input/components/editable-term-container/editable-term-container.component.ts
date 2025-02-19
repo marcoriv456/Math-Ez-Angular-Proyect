cbd602331196fa08a520dbb9237a50a23dedb976
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-18 19:24:35 -0600

    refactor: Editable term container refactoring
    Considering that the component is next tg become the only element capable of being edited manually it is no longer necessary that it extends from another class, so now it has all the properties the editable element base class has.
    
    Also it now doesn't need to get a parent reference as input, instead it takes it by itself using the  @SkipSelf decorator

--- src/app/input/components/editable-term-container/editable-term-container.component.ts
+++ src/app/input/components/editable-term-container/editable-term-container.component.ts
@@ -1,11 +1,85 @@
-import {Component} from '@angular/core';
-import {AuxiliaryEditableElement} from "../../classes/auxiliar-editable-element.class";
+import {Component, ElementRef, HostBinding, inject, Input, Optional, SkipSelf, ViewChild} from '@angular/core';
+import {TermContainerComponent} from "../term-container/term-container.component";
+import {Term} from "../../models/terms/term.model";
+import {InputMathElement} from "../../directives/input-math-element/input-math-element.abstract";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'editable-term-container',
   templateUrl: './editable-term-container.component.html',
   styleUrls: ['./editable-term-container.component.css','../../assets/editable-elements-styles.css']
 })
-export class EditableTermContainerComponent extends AuxiliaryEditableElement{
+export class EditableTermContainerComponent{
+  @Input() terms!:Term[]
+  @Input() index!:number
+
+  @ViewChild(TermContainerComponent) private termContainer!:TermContainerComponent
+  public caretIndex:number=-1
+
+  constructor(@SkipSelf() @Optional() public mathElement:InputMathElement<any>) {}
+
+  private readonly changeEmitter = new Subject<void>()
+  private readonly ref=inject(ElementRef).nativeElement as HTMLElement
+
+  @HostBinding('class.selected') public selected=false
+  @HostBinding('class.empty')
+  private get isEmpty(){
+    return this.terms && this.terms.length==0
+  }
+
+  public addChangesListener(callback:()=>void){
+    this.changeEmitter.subscribe(callback)
+  }
+
+  public append(terms:Term[], index:number){
+    this.terms.splice(index,0,...terms)
+    this.emitChange()
+  }
+
+  public replace(from:number, deleteCount=1,terms:Term[]){
+    this.terms.splice(from,deleteCount,...terms)
+    this.emitChange()
+  }
+
+  public delete(from:number,deleteCount=1){
+    this.terms.splice(from,deleteCount)
+    this.emitChange()
+  }
+
+  public getCharData(index:number){
+    return this.termContainer.renderedChars.get(index)?.data
+  }
+
+  public get noCharData(){
+    return {index:-1,positionX:this.positionX, parent:this}
+  }
+
+  public get lastCharData(){
+    return this.getCharData(this.termContainer.renderedChars.length-1)||this.noCharData
+  }
+
+  public get size():number{
+    return this.ref.offsetHeight
+  }
+
+  public get positionX(): number {
+    let parent=this.ref.parentElement
+    let leftPosition=this.ref.offsetLeft
+    while(parent && !(parent.tagName=='APP-INPUT')){
+      leftPosition+=parent.offsetLeft
+      parent=parent.parentElement
+    }
+    return leftPosition
+  }
+
+  public get clientY(): number {
+    return this.ref.getBoundingClientRect().top;
+  }
+
+  private emitChange(){
+    this.changeEmitter.next()
+  }
+
+
 
 }
