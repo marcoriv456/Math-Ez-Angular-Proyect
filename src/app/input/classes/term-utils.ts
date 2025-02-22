9ada20a1c206481e2d40cc1fc5a991fa103c75b6
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-22 13:09:06 -0600

    refactor: perform remover instructions util function created
    This function will be useful to perform remover tests

--- src/app/input/classes/term-utils.ts
+++ src/app/input/classes/term-utils.ts
@@ -1,5 +1,6 @@
 import {Term} from "../models/terms/term.model";
 import {TermAdderInstructions} from "../models/term-adder-instructions.model";
+import {TermRemoverInstructions} from "../models/term-remover-instructions.model";
 
 export class TermUtils {
   public static toString(terms:Term[]){
@@ -12,5 +13,9 @@ export class TermUtils {
 
   public static performAdderInstructions(terms:Term[], {replaceFrom,replaceCount,term}:TermAdderInstructions){
     terms.splice(replaceFrom,replaceCount,term)
+  }
+
+  public static performRemoverInstructions(terms:Term[], {index,count,remainingTerms}:TermRemoverInstructions){
+    terms.splice(index,count,...remainingTerms||[])
   }
 }
