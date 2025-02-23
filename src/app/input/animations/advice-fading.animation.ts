5519066df7659a40a2c7353a833fcd4fbca8cb05
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-22 23:56:36 -0600

    refactor: Advice fading animation moved to animations directory
    This animation was originally in the input component animations declarations

---
+++ src/app/input/animations/advice-fading.animation.ts
@@ -0,0 +1,11 @@
+import {animate, style, transition, trigger} from "@angular/animations";
+
+export const adviceFadingAnimation =     trigger('advice-fading',[
+  transition(':enter',[
+    style({transform:'translateY(-10%)',opacity:0}),
+    animate('500ms cubic-bezier(0,0,0,1)',style({transform:'translateY(0)',opacity:1}))
+  ]),
+  transition(':leave',[
+    animate('500ms cubic-bezier(0,0,0,1)',style({transform:'translateY(-10%)',opacity:0}))
+  ]),
+])
