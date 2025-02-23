86a84597fe2d5d1a78f5bba355c548d62d1a8eb6
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-22 23:10:48 -0600

    refactor: setTimeout removed from testing environment initialization

--- src/app/input/input-testing-environment/input-testing-environment.component.ts
+++ src/app/input/input-testing-environment/input-testing-environment.component.ts
@@ -8,11 +8,11 @@ import {InputModule} from "../input.module";
   selector: 'app-input-testing-environment',
   templateUrl: './input-testing-environment.component.html',
   styleUrl: './input-testing-environment.component.css',
-  standalone:true,
-  imports:[
+  standalone: true,
+  imports: [
     InputModule
   ],
-  providers:[
+  providers: [
     ExperimentalVariableProviderService
   ]
 })
@@ -23,63 +23,62 @@ export class InputTestingEnvironmentComponent implements AfterViewInit {
   @ViewChild(InputComponent) inputComponent!: InputComponent
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.inputComponent.variableHandler.setVariableProvider(this.expVariableProvider)
-      this.inputComponent.setTerms(
-        {char: '1', type: 'char'},
-        {char: '2', type: 'char'},
-        {char: '3', type: 'char'},
-        {char: '4', type: 'char'},
-        {char: '5', type: 'char'},
-        {char: '6', type: 'char'},
-        {char: '7', type: 'char'},
-        {char: '8', type: 'char'},
-        {char: '9', type: 'char'},
-        {char: ' ', type: 'char'},
-        {
-          numeratorChildren: [{char: '1', type: 'char'}, {char: '0', type: 'char'}],
-          denominatorChildren: [{char: '4', type: 'char'}],
-          type: 'fraction'
-        },
-        {char: ' ', type: 'char'},
-        {
-          functionName: 'log',
-          functionChildren: [
-            {char: '8', type: 'char'},
-            {char: '1', type: 'char'},
-          ],
-          argumentTerms: [{char: '9', type: 'char'}],
-          type: 'function'
-        },
-        {char: ' ', type: 'char'},
-        {
-          rootChildren: [
-            {char: '8', type: 'char'},
-            {char: '1', type: 'char'},
-          ],
-          radicalTerms: [{char: '9', type: 'char'}],
-          type: 'root'
-        },
-        {char: ' ', type: 'char'},
-        {
-          type: 'fraction',
-          numeratorChildren: [
-            {char: '1', type: 'char'}
-          ],
-          denominatorChildren: [
-            {char: '8', type: 'char'},
-          ]
-        },
-        {char: ' ', type: 'char'},
-        {char: '8', type: 'char'},
-        {
-          exponentChildren: [
-            {char: '2', type: 'char'},
-          ],
-          type: 'exponent'
-        }
-      )
-    })
+    this.inputComponent.variableHandler.setVariableProvider(this.expVariableProvider)
+    this.inputComponent.setTerms(
+      {char: '1', type: 'char'},
+      {char: '2', type: 'char'},
+      {char: '3', type: 'char'},
+      {char: '4', type: 'char'},
+      {char: '5', type: 'char'},
+      {char: '6', type: 'char'},
+      {char: '7', type: 'char'},
+      {char: '8', type: 'char'},
+      {char: '9', type: 'char'},
+      {char: ' ', type: 'char'},
+      {
+        numeratorChildren: [{char: '1', type: 'char'}, {char: '0', type: 'char'}],
+        denominatorChildren: [{char: '4', type: 'char'}],
+        type: 'fraction'
+      },
+      {char: ' ', type: 'char'},
+      {
+        functionName: 'log',
+        functionChildren: [
+          {char: '8', type: 'char'},
+          {char: '1', type: 'char'},
+        ],
+        argumentTerms: [{char: '9', type: 'char'}],
+        type: 'function'
+      },
+      {char: ' ', type: 'char'},
+      {
+        rootChildren: [
+          {char: '8', type: 'char'},
+          {char: '1', type: 'char'},
+        ],
+        radicalTerms: [{char: '9', type: 'char'}],
+        type: 'root'
+      },
+      {char: ' ', type: 'char'},
+      {
+        type: 'fraction',
+        numeratorChildren: [
+          {char: '1', type: 'char'}
+        ],
+        denominatorChildren: [
+          {char: '8', type: 'char'},
+        ]
+      },
+      {char: ' ', type: 'char'},
+      {char: '8', type: 'char'},
+      {
+        exponentChildren: [
+          {char: '2', type: 'char'},
+        ],
+        type: 'exponent'
+      }
+    )
+
 
   }
 }
