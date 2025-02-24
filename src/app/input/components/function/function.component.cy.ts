01b20dd553b20f7b8c0920737552ff8f9f6e9c6e
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-02-23 23:40:12 -0600

    test: Function component tests fixed

--- src/app/input/components/function/function.component.cy.ts
+++ src/app/input/components/function/function.component.cy.ts
@@ -1,6 +1,7 @@
 import {runEditableElementSuite} from "../../tests/run-editable-element-tests-suite.helper";
+import {functionArgumentSelector, functionBaseSelector} from "../../tests/test-selectors.util";
 
 describe('Function component: ', () => {
-  runEditableElementSuite(`Function argument`, `sen`, `function editable-term-container`)
-  runEditableElementSuite(`Function base`, `log`, `function argument`)
+  runEditableElementSuite(`Function argument`, `sen`, functionArgumentSelector)
+  runEditableElementSuite(`Function base`, `log`, functionBaseSelector)
 });
