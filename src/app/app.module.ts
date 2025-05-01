05daa515a95f086e9ad51c500aa525dd323fad3f
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-01 17:22:03 -0600

    refactor: Home page module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { InputComponent } from './input/input.component';
-import { InputTestingEnvironmentComponent } from './input/input-testing-environment/input-testing-environment.component';
 import {InputModule} from "./input/input.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { HomeComponent } from './ui/pages/home/home.component';
-import {NgxParticlesModule} from "@tsparticles/angular";
 import { FooterComponent } from './footer/footer.component';
 import {provideHttpClient} from "@angular/common/http";
+import {HomeModule} from "./ui/pages/home/home.module";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        HomeComponent,
         FooterComponent
     ],
     imports: [
@@ -25,7 +20,7 @@ import {provideHttpClient} from "@angular/common/http";
         AppRoutingModule,
         InputModule,
         BrowserAnimationsModule,
-        NgxParticlesModule,
+        HomeModule
     ],
     providers: [
       provideHttpClient()
---
+++ src/app/ui/pages/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HomeComponent} from "./home.component";
+import {InputModule} from "../../../input/input.module";
+import {NgxParticlesModule} from "@tsparticles/angular";
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    InputModule,
+    NgxParticlesModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class HomeModule { }
