7e60dd9ef47f9dd16b21fb56755687d278126645
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-01 20:21:54 -0600

    fix: Missed constructor syntax in EventBus

--- src/app/core/abstracts/event-bus.abstract.ts
+++ src/app/core/abstracts/event-bus.abstract.ts
@@ -3,7 +3,7 @@ import {filter, Subject} from "rxjs";
 export abstract class EventBus<E>{
   private eventSubject = new Subject<E>()
 
-  public on<T extends E>(eventType:{new (args:any[]):T}){
+  public on<T extends E>(eventType:new (...args:any[])=>T){
     return this.eventSubject.pipe(
       filter((event):event is T => event instanceof eventType)
     );
