8cea713154c2a220cd1c0b4fcf284aa5b148708f
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-12 20:30:49 -0600

    test: CaretIndexService tests created

--- src/app/ui/features/input/core/services/caret-index/caret-index.service.spec.ts
+++ src/app/ui/features/input/core/services/caret-index/caret-index.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CaretIndexService } from './caret-index.service';
+import {InputModule} from "../../../input.module";
 
 describe('CaretIndexService', () => {
   let service: CaretIndexService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [InputModule]});
     service = TestBed.inject(CaretIndexService);
   });
 
