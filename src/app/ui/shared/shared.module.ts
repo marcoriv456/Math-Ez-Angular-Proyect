bacf0c8e93b1da89b282f3d3f36613ec3813825c
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-23 12:53:57 -0600

    feat: Shared module created
    This module will contain all shared components present in almost every section of the app, such as buttons, links and, for this specific branch, the particles container.

---
+++ src/app/ui/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ParticlesComponent} from './atoms/particles/particles.component';
+
+
+@NgModule({
+  declarations: [
+    ParticlesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule {
+}
