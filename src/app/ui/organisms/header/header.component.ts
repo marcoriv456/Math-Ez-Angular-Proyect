c378d5afcc90eae157f68b9010ac93a3e7688e86
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-24 00:49:31 -0600

    fix: Detection cycle leak in HeaderComponent
    Due to the inclusion of flubber animations with svgs inside the component, the angular detection cycle was being executed every time the svg path was changing, which caused low performance issues sometimes.

--- src/app/ui/organisms/header/header.component.ts
+++ src/app/ui/organisms/header/header.component.ts
@@ -1,6 +1,4 @@
-import {AfterViewInit, Component, ElementRef, inject, OnDestroy} from '@angular/core';
-import {interpolate} from "flubber";
-import {FlubberAnimation} from "../../../core/helpers/flubber-animation.helper";
+import {AfterViewInit, Component, inject, NgZone, OnDestroy} from '@angular/core';
 import {BlobAnimation} from "../../../core/helpers/blob-animator.helper";
 
 @Component({
@@ -11,9 +9,13 @@ import {BlobAnimation} from "../../../core/helpers/blob-animator.helper";
 export class HeaderComponent implements AfterViewInit,OnDestroy{
   private bgBlobAnimation!:BlobAnimation;
 
+  private readonly ngZone = inject(NgZone)
+
   ngAfterViewInit() {
-    this.bgBlobAnimation=new BlobAnimation('header .background',3,4)
-    this.bgBlobAnimation.init({duration:3000,layerDelay:500})
+    this.ngZone.runOutsideAngular(() => {
+      this.bgBlobAnimation = new BlobAnimation('header .background', 3, 4)
+      this.bgBlobAnimation.init({duration: 3000, layerDelay: 500})
+    })
   }
   ngOnDestroy() {
     this.bgBlobAnimation.stop()
