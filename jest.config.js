40b80bea7f20a8c07dc19d12fa710e86e5ac4cdc
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-24 00:42:46 -0600

    fix: jest recognizes tsparticles
    The tsParticles wrapper tests were failing due to this missed configuration

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '^.+\\.(ts|mjs|html)$': 'ts-jest',
   },
   testMatch: ['**/__tests__/**/*.spec.ts', '**/*.spec.ts'],
-  transformIgnorePatterns: ['node_modules/(?!(@angular|rxjs|chai)/)'],
+  transformIgnorePatterns: ['node_modules/(?!(@angular|rxjs|chai|@tsparticles)/)'],
   moduleNameMapper: {
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
