2200d2a8e6801d1e399fc3f51e87e467646d62dc
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-26 13:13:18 -0600

    refactor: Created app styled h1 wrapper
    To prevent duplicated css rules in all parts of the page, this wrapper will store the styles a h1 header would use so it can be reused in any part of the page, the same is coming for other common text elements such as other header and paragraphs

---
+++ src/app/ui/shared/atoms/h1/h1.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ez-h1',
+  templateUrl: './h1.component.html',
+  styleUrl: './h1.component.css'
+})
+export class H1Component {
+
+}
