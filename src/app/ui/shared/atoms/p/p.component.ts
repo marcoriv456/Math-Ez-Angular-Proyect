402fe139975dbae898c9ef7e2d6ce4c9142a5dcc
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-27 09:56:12 -0600

    refactor: Created app styled p wrapper

---
+++ src/app/ui/shared/atoms/p/p.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ez-p',
+  templateUrl: './p.component.html',
+  styleUrl: './p.component.css'
+})
+export class PComponent {
+
+}
--- src/app/ui/shared/shared.module.ts
+++ src/app/ui/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ParticlesComponent} from './atoms/particles/particles.component';
+import {H1Component} from './atoms/h1/h1.component';
+import {PComponent} from './atoms/p/p.component';
 
 
 @NgModule({
   declarations: [
-    ParticlesComponent
+    ParticlesComponent,
+    H1Component,
+    PComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ParticlesComponent
+    ParticlesComponent,
+    H1Component,
+    PComponent
   ]
 })
 export class SharedModule {
