7ca41fabeb4a5b2155d43b476935a302a43f23d0
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-27 12:46:37 -0600

    test: GlobalEventBus tests created

--- src/app/core/services/global-event-bus/global-event-bus.service.spec.ts
+++ src/app/core/services/global-event-bus/global-event-bus.service.spec.ts
@@ -1,6 +1,8 @@
 import {TestBed} from '@angular/core/testing';
 
 import {GlobalEventBusService} from './global-event-bus.service';
+import {FooterIntersectedEvent} from "../../models/events/footer-intersected.event";
+import {Observable} from "rxjs";
 
 describe('GlobalEventBusService', () => {
   let service: GlobalEventBusService;
@@ -13,4 +15,14 @@ describe('GlobalEventBusService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(`Returns an observable which can be used to listen to a determined type of events`, () => {
+    const observer = jest.fn()
+
+    service.on(FooterIntersectedEvent).subscribe(observer)
+    service.emit(new FooterIntersectedEvent(false))
+
+    expect(observer).toHaveBeenCalledWith(expect.any(Observable));
+  });
+
 });
