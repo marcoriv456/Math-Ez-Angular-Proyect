b3f17cbd93c710f480c00fc3143d33c0f83fe22d
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-05-27 10:01:15 -0600

    feat: h2 text wrapper created

---
+++ src/app/ui/shared/atoms/h2/h2.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ez-h2',
+  templateUrl: './h2.component.html',
+  styleUrl: './h2.component.css'
+})
+export class H2Component {
+
+}
