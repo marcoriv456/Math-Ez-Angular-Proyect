a1e39c4888ed35e8b7a56250c9d45f1528cc9b9e
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-02 17:25:23 -0600

    feat: Add CalculatorsComponent and update routing to include it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import {
   InputTestingEnvironmentComponent
 } from "./ui/features/input/tests/input-testing-environment/input-testing-environment.component";
 import {HomeComponent} from "./pages/home/home.component";
+import {CalculatorsComponent} from "./pages/calculators/calculators.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'calculators',
+    component: CalculatorsComponent
   },
   {
     path:'input-testing',
---
+++ src/app/pages/calculators/calculators.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CalculatorsComponent} from './calculators.component';
+
+
+@NgModule({
+  declarations: [
+    CalculatorsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CalculatorsModule {
+}
