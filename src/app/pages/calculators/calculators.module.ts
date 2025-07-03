6f6d2ac33c2b5cc63521841e7ee6f104e8ccd724
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-02 20:03:50 -0600

    feat: Add IntroductionComponent and update CalculatorsComponent template

--- src/app/pages/calculators/calculators.module.ts
+++ src/app/pages/calculators/calculators.module.ts
@@ -1,14 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CalculatorsComponent} from './calculators.component';
+import {IntroductionComponent} from './sections/introduction/introduction.component';
+import {SharedModule} from "../../ui/shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    CalculatorsComponent
+    CalculatorsComponent,
+    IntroductionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+  ],
+  exports: [
+    CalculatorsComponent
   ]
 })
 export class CalculatorsModule {
---
+++ src/app/pages/calculators/sections/introduction/introduction.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'calculators-introduction',
+  templateUrl: './introduction.component.html',
+  styleUrl: './introduction.component.css'
+})
+export class IntroductionComponent {
+
+}
