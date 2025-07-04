51af6ebac24ba3b676a6249956cb032a3fc46ce8
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-03 21:34:45 -0600

    feat: Integrate GlobalEventBus for color schema updates in calculators and header components

--- src/app/pages/calculators/calculators.component.ts
+++ src/app/pages/calculators/calculators.component.ts
@@ -1,5 +1,8 @@
-import {AfterViewInit, Component, QueryList, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, inject, QueryList, ViewChildren} from '@angular/core';
 import {BlobComponent} from "../../ui/shared/atoms/blob/blob.component";
+import {GlobalEventBusService} from "../../core/services/global-event-bus/global-event-bus.service";
+import {ColorSchemaChangeEvent} from "../../core/models/events/color-schema-change.event";
+import {ColorSchemas} from "../../core/config/color-schemas.config";
 
 @Component({
   selector: 'app-calculators',
@@ -9,8 +12,11 @@ import {BlobComponent} from "../../ui/shared/atoms/blob/blob.component";
 export class CalculatorsComponent implements AfterViewInit {
   @ViewChildren(BlobComponent) blobs!: QueryList<BlobComponent>;
 
-  async ngAfterViewInit() {
+  private readonly globalEventBus = inject(GlobalEventBusService)
+
+  ngAfterViewInit() {
     this.blobs.forEach(blob => blob.show())
+    this.globalEventBus.emit(new ColorSchemaChangeEvent(ColorSchemas.ALTERNATE))
   }
 
 }
--- src/app/ui/organisms/header/header.component.ts
+++ src/app/ui/organisms/header/header.component.ts
@@ -1,6 +1,10 @@
-import {AfterViewInit, Component, inject, NgZone, OnDestroy} from '@angular/core';
+import {AfterViewInit, Component, HostBinding, inject, NgZone, OnDestroy} from '@angular/core';
 import {BlobAnimation} from "../../../core/helpers/blob-animator.helper";
 import {SharedModule} from "../../shared/shared.module";
+import {GlobalEventBusService} from "../../../core/services/global-event-bus/global-event-bus.service";
+import {ColorSchema} from "../../../core/models/color-schema.model";
+import {ColorSchemas} from "../../../core/config/color-schemas.config";
+import {ColorSchemaChangeEvent} from "../../../core/models/events/color-schema-change.event";
 
 @Component({
   selector: 'app-header',
@@ -12,17 +16,37 @@ import {SharedModule} from "../../shared/shared.module";
   ]
 })
 export class HeaderComponent implements AfterViewInit,OnDestroy{
+  @HostBinding('style') schema = this.transformSchemaToCssVariables(ColorSchemas.DEFAULT);
+
   private bgBlobAnimation!:BlobAnimation;
 
   private readonly ngZone = inject(NgZone)
+  private readonly globalEventBus = inject(GlobalEventBusService);
 
   ngAfterViewInit() {
     this.ngZone.runOutsideAngular(() => {
       this.bgBlobAnimation = new BlobAnimation('header .background', 3, 4)
       this.bgBlobAnimation.init({duration: 3000, layerDelay: 500})
     })
+
+    this.globalEventBus.on(ColorSchemaChangeEvent).subscribe(event => {
+      this.changeColorSchema(event.newColorSchema);
+    });
   }
   ngOnDestroy() {
     this.bgBlobAnimation.stop()
+  }
+
+  private changeColorSchema(schema: ColorSchema) {
+    this.schema = this.transformSchemaToCssVariables(schema);
+  }
+
+  private transformSchemaToCssVariables(schema: ColorSchema) {
+    return {
+      '--layer-1-fill-color': schema.main,
+      '--layer-2-fill-color': schema.dark1,
+      '--layer-3-fill-color': schema.dark2,
+      '--layer-4-fill-color': schema.dark3,
+    }
   }
 }
