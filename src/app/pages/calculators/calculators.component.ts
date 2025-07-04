c62bac3c69a59361083a9d9c91e413adde8a5107
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-04 00:06:12 -0600

    feat: Inject ChangeDetectorRef and trigger change detection after updating blob colors

--- src/app/pages/calculators/calculators.component.ts
+++ src/app/pages/calculators/calculators.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, inject, QueryList, ViewChildren} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, inject, QueryList, ViewChildren} from '@angular/core';
 import {BlobComponent} from "../../ui/shared/atoms/blob/blob.component";
 import {GlobalEventBusService} from "../../core/services/global-event-bus/global-event-bus.service";
 import {ColorSchemaChangeEvent} from "../../core/models/events/color-schema-change.event";
@@ -13,12 +13,14 @@ export class CalculatorsComponent implements AfterViewInit {
   @ViewChildren(BlobComponent) blobs!: QueryList<BlobComponent>;
 
   private readonly globalEventBus = inject(GlobalEventBusService)
+  private readonly cdr = inject(ChangeDetectorRef)
 
   ngAfterViewInit() {
     this.blobs.forEach(blob => {
       blob.changeColors(ColorSchemas.ALTERNATE)
       blob.show()
     })
+    this.cdr.detectChanges();
     this.globalEventBus.emit(new ColorSchemaChangeEvent(ColorSchemas.ALTERNATE))
   }
 
