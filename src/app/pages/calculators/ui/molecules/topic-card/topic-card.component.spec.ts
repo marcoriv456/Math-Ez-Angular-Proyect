64971572b391a338641c080ea0c34aab6c6391e2
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-03 23:36:57 -0600

    feat: Add TopicCardComponent with template, styles, and basic tests

---
+++ src/app/pages/calculators/ui/molecules/topic-card/topic-card.component.spec.ts
@@ -0,0 +1,36 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {TopicCardComponent} from './topic-card.component';
+import {CalculatorsModule} from "../../../calculators.module";
+
+describe('TopicCardComponent', () => {
+  let component: TopicCardComponent;
+  let fixture: ComponentFixture<TopicCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CalculatorsModule]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(TopicCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should apply the default card color when no color is provided', () => {
+    const hostElement = fixture.nativeElement;
+    expect(hostElement.style.getPropertyValue('--card-color')).toBe('hsl(200, 100%, 50%)');
+  });
+
+  it('should apply the provided card color', () => {
+    component.color = 'hsl(100, 50%, 50%)';
+    fixture.detectChanges();
+    const hostElement = fixture.nativeElement;
+    expect(hostElement.style.getPropertyValue('--card-color')).toBe('hsl(100, 50%, 50%)');
+  });
+});
---
+++ src/app/pages/calculators/ui/molecules/topic-card/topic-card.component.ts
@@ -0,0 +1,10 @@
+import {Component, HostBinding, Input} from '@angular/core';
+
+@Component({
+  selector: 'calculators-topic-card',
+  templateUrl: './topic-card.component.html',
+  styleUrl: './topic-card.component.css'
+})
+export class TopicCardComponent {
+  @Input() @HostBinding("style.--card-color") color: string = 'hsl(200, 100%, 50%)';
+}
