d66636b95b10da1bc224d0627251fe1f5d51e27d
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-03 21:17:30 -0600

    feat: Add ColorSchemaChangeEvent for handling color schema update requests

---
+++ src/app/core/models/events/color-schema-change.event.ts
@@ -0,0 +1,7 @@
+import {GlobalEvent} from "./base/global-event.model";
+import {ColorSchema} from "../color-schema.model";
+
+export class ColorSchemaChangeEvent implements GlobalEvent {
+  constructor(public readonly newColorSchema: ColorSchema) {
+  }
+}
