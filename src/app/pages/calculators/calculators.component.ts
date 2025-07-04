fdde56b59605ccceeb61a0691d5f1a21d0b14b9f
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-03 20:31:50 -0600

    feat: Add multiple BlobComponents to calculators with dynamic visibility

--- src/app/pages/calculators/calculators.component.ts
+++ src/app/pages/calculators/calculators.component.ts
@@ -1,10 +1,16 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, QueryList, ViewChildren} from '@angular/core';
+import {BlobComponent} from "../../ui/shared/atoms/blob/blob.component";
 
 @Component({
   selector: 'app-calculators',
   templateUrl: './calculators.component.html',
   styleUrl: './calculators.component.css'
 })
-export class CalculatorsComponent {
+export class CalculatorsComponent implements AfterViewInit {
+  @ViewChildren(BlobComponent) blobs!: QueryList<BlobComponent>;
+
+  async ngAfterViewInit() {
+    this.blobs.forEach(blob => blob.show())
+  }
 
 }
