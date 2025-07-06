5041d4223281bc29ae7e2dad6a715249f04251af
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-05 21:02:50 -0600

    feat: Update footer component styles and add dynamic background color

--- src/app/ui/organisms/footer/footer.component.ts
+++ src/app/ui/organisms/footer/footer.component.ts
@@ -1,13 +1,18 @@
-import {AfterViewInit, Component, ElementRef, inject} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostBinding, inject} from '@angular/core';
 import {GlobalEventBusService} from "../../../core/services/global-event-bus/global-event-bus.service";
 import {FooterIntersectedEvent} from "../../../core/models/events/footer-intersected.event";
+import {ColorSchemaChangeEvent} from "../../../core/models/events/color-schema-change.event";
+import {ColorSchemas} from "../../../core/config/color-schemas.config";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
+  standalone: true
 })
 export class FooterComponent implements AfterViewInit{
+  @HostBinding("style.--footer-bg") color = ColorSchemas.DEFAULT.dark3
+
   private ref=inject(ElementRef).nativeElement as HTMLElement
   private readonly eventBus = inject(GlobalEventBusService)
 
@@ -15,6 +20,7 @@ export class FooterComponent implements AfterViewInit{
 
   ngAfterViewInit() {
     this.observer.observe(this.ref)
+    this.eventBus.on(ColorSchemaChangeEvent).subscribe(event => this.color = event.newColorSchema.dark3)
   }
 
 }
