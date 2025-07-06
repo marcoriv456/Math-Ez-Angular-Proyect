940773dfa116969b0dc5ea60a4bcf8a75f44f4e2
Marcos Nicolas Ramirez Rivera <marcosnicolasrivera456@gmail.com>
2025-07-05 21:56:56 -0600

    feat: Add dynamic color events to the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,19 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, inject} from '@angular/core';
 import {bgConfig} from "./core/config/bg-config2.config";
+import {GlobalEventBusService} from "../../core/services/global-event-bus/global-event-bus.service";
+import {ColorSchemaChangeEvent} from "../../core/models/events/color-schema-change.event";
+import {ColorSchemas} from "../../core/config/color-schemas.config";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   protected readonly bgConfig = bgConfig;
+  private readonly eventBus = inject(GlobalEventBusService)
+
+  ngAfterViewInit() {
+    this.eventBus.emit(new ColorSchemaChangeEvent(ColorSchemas.DEFAULT))
+  }
 }
